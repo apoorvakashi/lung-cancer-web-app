f38cd94a013c9091b975fc3dd5631ca289060405
Mohammed Misran <mohammed.misran38@gmail.com>
2022-06-09 20:58:43 +0530

    changed repository link

--- client/src/components/Common/Footer.js
+++ client/src/components/Common/Footer.js
@@ -1,5 +1,6 @@
 import { AppBar, Link, Toolbar, Typography } from '@mui/material';
 import React from 'react';
+import { REPOSITORY_LINK } from '../../constants/constants';
 import { appBarStyle, toolbarStyle } from './FooterStyle';
 
 const Footer = () => {
@@ -7,7 +8,7 @@ const Footer = () => {
     <AppBar component="footer" sx={appBarStyle}>
       <Toolbar sx={toolbarStyle}>
         <Typography>2022 &#169; ISE-B14-2022 Team</Typography>
-        <Link color="primary.contrastText" href="https://www.github.com/" target="_blank">
+        <Link color="primary.contrastText" href={REPOSITORY_LINK} target="_blank">
           Repository Link
         </Link>
       </Toolbar>
